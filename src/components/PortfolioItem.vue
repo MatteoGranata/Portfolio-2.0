6c13408ff30fd7b981c30b6bd9cc18bc292954a2
matteo granata <matteogranata92@gmail.com>
2024-09-24 18:38:43 +0200

    added animation for portfolio components

--- src/components/PortfolioItem.vue
+++ src/components/PortfolioItem.vue
@@ -1,126 +1,33 @@
-<!-- <template>
-    <div class="w-full h-screen overflow-hidden relative">
-        <div class="horizontal-container bg-black w-screen h-full relative border">
-            <MouseEvent />
-            <section v-for="(project, index) in projects" :key="index"
-                class="panel w-4/6 h-fit lala flex flex-col -z-20 justify-center items-center absolute top-[-42rem] left-[119rem] border border-blue-800 bg-transparent">
-                <div class="text-white border h-fit w-fit flex items-end text-[5rem] uppercase justify-center">
-                    {{ project.description }}
-                </div>
-                <div class="project flex justify-center h-fit items-center w-fit">
-                    <video :src="project.video" class="w-full h-fit" autoplay muted loop></video>
-                </div>
-            </section>
-        </div>
-    </div>
-</template>
-
-<script>
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import MouseEvent from "./MouseEvent.vue";
-import videoAngel from "../assets/video/Registrazione_Angel.mp4";
-
-// Registra il plugin ScrollTrigger
-gsap.registerPlugin(ScrollTrigger);
-
-export default {
-    components: {
-        MouseEvent
-    },
-    name: "ImageHoverVideo",
-    data() {
-        return {
-            projects: [
-                { video: videoAngel, description: 'Project 1' },
-                { video: videoAngel, description: 'Project 2' },
-                { video: videoAngel, description: 'Project 3' },
-                { video: videoAngel, description: 'Project 4' },
-                { video: videoAngel, description: 'Project 5' },
-                { video: videoAngel, description: 'Project 6' },
-                { video: videoAngel, description: 'Project 7' },
-                { video: videoAngel, description: 'Project 8' },
-                { video: videoAngel, description: 'Project 9' },
-                { video: videoAngel, description: 'Project 10' }
-            ]
-        };
-    },
-    mounted() {
-        const sections = gsap.utils.toArray(".panel");
-        const totalSections = sections.length;
-        const scrollLength = totalSections * 100;
-
-        sections.forEach((section, index) => {
-            gsap.to(section,
-                {
-                    x: "-235rem",  // Si muove in diagonale a destra
-                    y: 2000, // Si muove in diagonale verso l'alto
-                    duration: 2,
-                    zIndex: "=+1", // Durata del movimento
-                    ease: "none",
-                    scrollTrigger: {
-
-                        trigger: section, // Trigger per tutto il container
-                        start: `top+=${index * 100}% top`, // Ogni sezione inizia a comparire più rapidamente
-                        end: `top+=${(index + 3) * 100}% top`, // Sovrapposizione aumentata tra le sezioni
-                        scrub: true,
-                        markers: true, // Marker visibili per debug
-                    }
-                }
-            );
-        });
-
-
-        // Mantieni lo sfondo "pinnato"
-        ScrollTrigger.create({
-            trigger: ".horizontal-container",
-            start: "top top",
-            end: `+=${scrollLength}%`, // Imposta la lunghezza totale dello scroll
-            pin: true,
-            scrub: true,
-            markers: true
-        });
-    },
-    beforeUnmount() {
-        ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-    }
-};
-</script> -->
-
-
-
-
-
-
-
-
-
-
-
 <template>
-    <div class="w-full h-fit flex justify-center bg-black">
-
-        <div class="flex flex-col ">
+    <div class="font-[League-Spartan] w-full h-fit flex justify-center bg-[#0f0f0f]">
+        <div class="flex flex-col">
             <div v-for="(image, index) in images" :key="index"
-                class="h-screen flex flex-col justify-center items-center relative" :ref="'section' + index">
+                class="section h-screen flex flex-col justify-center items-center relative  bg-[#0f0f0f] ">
                 <MouseEvent />
-                <!-- Titolo dell'immagine -->
-                <div class="text-white text-center text-[2oem] font-bold border" :ref="'title' + index">{{ titles[index]
-                    }}
+                <div class="flex title items-center justify-center w-full h-12 rounded-lg mb-5 bg-[#F0ECE2]">
+                    <!-- Titolo dell'immagine -->
+                    <div class=" text-[#000000] text-center text-[1.5rem] font-bold w-full h-fit">
+                        {{
+                            titles[index] }}
+                    </div>
                 </div>
-
                 <!-- Immagine -->
-                <video :src="image" :alt="titles[index]" class="image border w-full h-full object-cover "
-                    :ref="'image' + index" autoplay muted loop></video>
+                <video :src="image" :alt="titles[index]" class="image w-full h-full object-cover rounded-lg" autoplay
+                    muted loop></video>
             </div>
         </div>
     </div>
 </template>
 
+
 <script>
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import AngelImage from "../assets/video/Registrazione_Angel.mp4"
+import CounterVideo from "../assets/video/Registrazione_Contatore.mp4"
+import ReadBooksVideo from "../assets/video/Registrazione_ReadBooks.mp4"
+import SunneeVideo from "../assets/video/Registrazione_Schermo_Sunnee.mp4"
+import TicTacToeVideo from "../assets/video/Registrazione_Tic-Tac-Toe.mp4"
 import MouseEvent from "./MouseEvent.vue";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -133,11 +40,11 @@ export default {
     data() {
         return {
             titles: [
-                "Image 1",
-                "Image 2",
-                "Image 3",
-                "Image 4",
-                "Image 5",
+                "Tic Tac Toe Games",
+                "ReadBooks",
+                "Counter",
+                "Sunnee",
+                "Angel",
                 "Image 6",
                 "Image 7",
                 "Image 8",
@@ -145,10 +52,10 @@ export default {
                 "Image 10",
             ],
             images: [
-                AngelImage,
-                AngelImage,
-                AngelImage,
-                AngelImage,
+                TicTacToeVideo,
+                ReadBooksVideo,
+                CounterVideo,
+                SunneeVideo,
                 AngelImage,
                 AngelImage,
                 AngelImage,
@@ -163,31 +70,30 @@ export default {
     },
     methods: {
         createScrollAnimation() {
-            this.titles.forEach((title, index) => {
-                const section = this.$refs['section' + index][0];
-                const img = this.$refs['image' + index][0];
+            const sections = document.querySelectorAll('.section');
+            const images = document.querySelectorAll('.image');
+
+            sections.forEach((section, index) => {
+                const img = images[index];
 
                 gsap.timeline({
                     scrollTrigger: {
                         trigger: section,
-                        start: "center center",
+                        start: "top top",
                         end: "bottom top",
                         scrub: true,
-                        markers: true,
                         pin: true,
-                        opacity: 0,
+                        markers: true,
                     },
-                }).fromTo(
-                    img,
-                    { height: '100vh', objectFit: 'cover' },
-                    {
-                        height: '0vh', duration: 1, scrollTrigger: {
-                            pin: true
-                        }
-                    }
-                )
+                })
+                    .fromTo(
+                        img,
+                        { height: '80%', objectFit: 'cover' },
+                        { height: '0vh', duration: 1 }
+                    )
             });
         },
-    },
+    }
+
 };
 </script>
