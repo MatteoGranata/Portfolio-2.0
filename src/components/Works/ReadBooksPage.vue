509dd3f7f16028882659462c4efc05dafec97c0d
matteo granata <matteogranata92@gmail.com>
2024-11-07 13:17:14 +0100

    Fixed Responsive images

--- src/components/Works/ReadBooksPage.vue
+++ src/components/Works/ReadBooksPage.vue
@@ -12,8 +12,8 @@
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-y-10 lg:gap-60 py-10 px-5 lg:px-10 justify-center">
             <!-- Immagine grande che occupa tutte e 4 le colonne -->
             <div
-                class="w-full rounded-2xl flex items-center h-[20rem] lg:h-[50rem] overflow-hidden col-span-2 place-self-center">
-                <img :src="mockup" class="object-cover w-full h-[30rem] lg:h-full">
+                class="w-full rounded-2xl flex items-center h-[20rem] lg:h-[60rem] overflow-hidden col-span-2 place-self-center">
+                <img :src="mockup" class="object-cover w-full h-[20rem] lg:h-full">
             </div>
             <div class="h-fit w-full">
                 <img :src="readbooksScreen" class="rounded-xl object-cover w-full">
--- src/components/Works/TicTacToePage.vue
+++ src/components/Works/TicTacToePage.vue
@@ -9,7 +9,7 @@
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-y-10 lg:gap-60 py-10 px-5 lg:px-10 justify-center">
             <!-- Immagine grande che occupa tutte e 4 le colonne -->
             <div
-                class="w-full rounded-2xl flex items-center h-[20rem] lg:h-[50rem] overflow-hidden col-span-2 place-self-center">
+                class="w-full rounded-2xl flex items-center h-[20rem] lg:h-[55rem] overflow-hidden col-span-2 place-self-center">
                 <img :src="mockup" class="object-cover w-full h-[50rem] lg:h-full">
             </div>
             <div class="h-fit w-full">
