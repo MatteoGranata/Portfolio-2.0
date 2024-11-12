1080be8da23c73bf72ef519b91800b25415d4510
matteo granata <matteogranata92@gmail.com>
2024-11-12 14:16:59 +0100

    Changed style and bio

--- src/views/FooterPage.vue
+++ src/views/FooterPage.vue
@@ -1,53 +1,59 @@
 <template>
-  <div class="w-full h-full mt-0 pt-20 font-[Roboto] about-section">
+  <div class="w-full h-full mt-0 pt-20 px-5 font-[Roboto] about-section">
     <div class="grid grid-cols-1 lg:grid-cols-2 px-3 lg:px-3 lg:p-3 mb-5 lg:mb-0">
-      <div class="w-full order-2 lg:order-1">
-        <div class="border-t-2 w-11/12">
-          <h1 class="text-[2rem] md:text-[2.5rem] mb-5 lg:mb-10">Skills</h1>
-          <div class="grid grid-cols-2 gap-2 lg:gap-5 text-[1rem] md:text-[1.4rem]">
-            <p class="border h-fit w-fit rounded-xl py-1 px-3">HTML</p>
-            <p class="border h-fit w-fit rounded-xl py-1 px-3">CSS</p>
-            <p class="border h-fit w-fit rounded-xl py-1 px-3">JavaScript</p>
-            <p class="border h-fit w-fit rounded-xl py-1 px-3">Vue3.js</p>
-            <p class="border h-fit w-fit rounded-xl py-1 px-3">Node</p>
-            <p class="border h-fit w-fit rounded-xl py-1 px-3">TailwindCSS</p>
-            <p class="border h-fit w-fit rounded-xl py-1 px-3">GSAP</p>
-            <p class="border h-fit w-fit rounded-xl py-1 px-3">Bootstrap</p>
-          </div>
+      <div class="border-t-2 w-11/12 order-2 lg:order-1">
+        <h1 class="text-[2rem] md:text-[2.5rem] mb-5 lg:mb-10">SKILLS</h1>
+        <div class="grid grid-cols-2 gap-2 lg:gap-7 text-[1rem] md:text-[1.4rem]">
+          <p class=" h-fit w-fit rounded-xl py-1 px-3">&#10141; HTML</p>
+          <p class=" h-fit w-fit rounded-xl py-1 px-3">&#10141; CSS</p>
+          <p class=" h-fit w-fit rounded-xl py-1 px-3">&#10141; JavaScript</p>
+          <p class=" h-fit w-fit rounded-xl py-1 px-3">&#10141; Vue3.js</p>
+          <p class=" h-fit w-fit rounded-xl py-1 px-3">&#10141; Node</p>
+          <p class=" h-fit w-fit rounded-xl py-1 px-3">&#10141; TailwindCSS</p>
+          <p class=" h-fit w-fit rounded-xl py-1 px-3">&#10141; GSAP</p>
+          <p class=" h-fit w-fit rounded-xl py-1 px-3">&#10141; Bootstrap</p>
         </div>
       </div>
-      <div class="border-t-2 order-1 lg:order-2 lg:p-3 px-3 mb-5 lg:px-0 lg:mb-0">
-        <h1 class="text-[2rem] md:text-[2.5rem] mb-5 lg:mb-10">Bio</h1>
-        <p class="text-[1.3rem] md:text-[1.8rem]">
-          Sono un Full-Stack Developer italiano di Sesto San Giovanni, provincia di Milano, con una
-          formazione in grafica pubblicitaria e una forte passione per la programmazione. Ho
-          completato un corso di programmazione full-stack presso start2impact, sviluppando
-          competenze in JavaScript, Node.js, Express e Vue.js. Sono motivato a contribuire al
-          settore tecnologico con soluzioni innovative, utilizzando tecnologie avanzate e
-          affrontando nuove sfide per crescere professionalmente. Il mio obiettivo è migliorare
-          continuamente le mie competenze tecniche e collaborare a progetti di grande impatto.
-        </p>
+      <div class="border-t-2 order-1 lg:order-2 mb-5 lg:mb-0">
+        <h1 class="text-[2rem] md:text-[2.5rem] mb-5 lg:mb-10">BIO</h1>
+        <div class="text-[1.2rem] md:text-[1.6rem] font-[Roboto] leading-relaxed">
+          <p>
+            Sono un <span class="font-semibold">Full-Stack Developer</span> con una formazione in
+            <span class="font-semibold">grafica pubblicitaria</span> e una forte passione per la programmazione.
+            Ho completato un corso full-stack presso
+            <a href="https://www.start2impact.it/" class="underline hover:text-[#ddc88f]">start2impact</a>.
+          </p>
+          <p class="mt-4">
+            Durante il mio percorso, ho realizzato progetti personali che esprimono il mio impegno nel creare
+            soluzioni innovative e pratiche. Sono appassionato di tecnologie avanzate e desidero contribuire al
+            settore con soluzioni d’impatto. Il mio obiettivo è crescere continuamente e collaborare su progetti
+            significativi.
+          </p>
+        </div>
       </div>
     </div>
     <div class="w-full h-full px-3 md:p-3">
       <div class="w-[99%] h-fit border-t-2 py-2 md:py-0">
-        <a
-          class="text-[1.8rem] md:text-[4rem]"
-          target="_blank"
-          href="mailto:portfolio.granata@gmail.com"
-        >
-          portfolio.granata@gmail.com
+        <a class="text-[1.8rem] md:text-[4rem]" target="_blank" href="mailto:granata.matteo.j@gmail.com">
+          granata.matteo.j@gmail.com
         </a>
       </div>
     </div>
     <div class="w-full h-full px-3 md:p-3">
-      <div class="w-[99%] h-fit border-t-2 text-[1.1rem] md:text-[2rem] py-5">
-        <a class="mr-5" target="_blank" href="https://www.linkedin.com/in/granata-matteo/">
-          Linkedin
-        </a>
-        <a class="ml-5" target="_blank" href="https://github.com/MatteoGranata"> GitHub </a>
+      <div class="w-[99%] h-fit border-t-2 text-[1.1rem] md:text-[2rem] py-5 flex justify-between">
+        <div>
+          <span> &#10141;</span> <a class="mr-5 hover:underline" target="_blank"
+            href="https://www.linkedin.com/in/granata-matteo/">
+            Linkedin
+          </a>
+          <span class="ml-5">&#10141;</span> <a class="hover:underline" target="_blank"
+            href="https://github.com/MatteoGranata">GitHub</a>
+        </div>
+        <p> © 2024</p>
       </div>
+
     </div>
+
   </div>
 </template>
 
